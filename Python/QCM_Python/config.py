149f8eeed11be9b9bd498308a4208c3202e931f7
Hugo <durand.hugo.021@gmail.com>
2022-06-01 14:13:42 +0200

    réadaptation du dossier

--- Python/QCM_Python/config.py
+++
@@ -1,8 +0,0 @@
-import os
-config = {
-    'user' : os.getenv('USER_DB'),
-    'password' : os.getenv('DB_PASSWORD'),
-    'host': os.getenv('DB_HOST'),
-    'port': int(os.getenv('DB_PORT')),
-    'database': os.getenv('DB_NAME')
-}
