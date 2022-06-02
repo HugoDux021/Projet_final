c952aeba7898d8f65cbead80fd7d24f5b207f079
HugoDux021 <103499198+HugoDux021@users.noreply.github.com>
2022-06-02 15:41:57 +0200

    Update test.py

--- files/app-test/test.py
+++ files/app-test/test.py
@@ -2,7 +2,7 @@ import pytest
 import sys, os
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
-import /../app-python/lancement_QCM.py as lance_QCM
+import /var/jenkins_home/workspace/pipelineprojet/files/app-python/lancement_QCM.py as lance_QCM
 
 def test_get_nom():
     assert lance_QCM.hello('lucie') == 'Hello lucie'
