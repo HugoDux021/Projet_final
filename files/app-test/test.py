50c4dc5bebcbaffacbf025f1dca18e1051faf2cf
HugoDux021 <103499198+HugoDux021@users.noreply.github.com>
2022-06-02 15:52:31 +0200

    Update test.py

--- files/app-test/test.py
+++ files/app-test/test.py
@@ -1,8 +1,23 @@
 import pytest
 import sys, os
-myPath = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, myPath + '/../')
-import lancement_QCM.py as lance_QCM
+  
+cwd = "/home/dux/Documents/Formation/Projet/files/app-python"
+  
+fd = 'false_dir / temp'
+  
+try: 
+    os.chdir(fd) 
+    print("Inserting inside-", os.getcwd()) 
+      
+except: 
+    print("Something wrong with specified directory. Exception- ", sys.exc_info()) 
+            
+finally: 
+    print("Restoring the path") 
+    os.chdir(cwd) 
+    print("Current directory is-", os.getcwd()) 
+
+import lancement_QCM as lance_QCM
 
 def test_get_nom():
     assert lance_QCM.hello('lucie') == 'Hello lucie'
